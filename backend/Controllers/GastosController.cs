2a5038e499b21462ef923f7c53e14e90b1cc8e79
Joel <joelstivala@gmail.com>
2025-12-14 18:43:06 -0300

    Se agregó la clase GastosController y el endpoint GET

---
+++ backend/Controllers/GastosController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using backend.Models;
+using backend.Repositories.GastoRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class GastosController : ControllerBase
+    {
+        private readonly IGastoRepository _gastoRepo;
+
+        public GastosController(IGastoRepository gastoRepo)
+        {
+            _gastoRepo = gastoRepo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var gastos = await _gastoRepo.GetAllAsync();
+
+            return Ok(gastos);
+        }
+    }
+}
