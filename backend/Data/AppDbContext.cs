d635cd14af9dd556933f0e0a1babdf30bf5809e2
Joel <joelstivala@gmail.com>
2025-12-24 10:20:36 -0300

    Se agregó la tabla Categoria al DbContext

--- backend/Data/AppDbContext.cs
+++ backend/Data/AppDbContext.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using backend.Controllers;
 using backend.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -10,12 +11,23 @@ namespace backend.Data
 {
     public class AppDbContext : IdentityDbContext
     {
+
+        public DbSet<Gasto> Gastos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
         public AppDbContext(DbContextOptions dbContextOptions)
         : base(dbContextOptions)
         {
             
         }
 
-        public DbSet<Gasto> Gastos { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Gasto>()
+                .HasOne(g => g.Categoria)
+                .WithMany(c => c.Gastos)
+                .HasForeignKey(g => g.IdCategoria)
+                .IsRequired();
+        }
     }
 }
